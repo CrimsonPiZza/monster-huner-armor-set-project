71a767beca5d19a4d4449faa7184bd25e135bc4f
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2021-02-24 16:50:59 +0700

    Add prop to Fashion Card

--- src/components/FashionCard.vue
+++ src/components/FashionCard.vue
@@ -1,49 +1,69 @@
 <template>
-	<mdb-container>
-		<mdb-row>
-			<mdb-col sm="4">
-				<mdb-card class="bg-dark text-light">
-					<mdb-view hover>
-						<a href="#!">
-							<mdb-card-image src="https://i.ibb.co/3MNqtqX/Dr12aio-Vs-AATR-T.jpg" alt="Card image cap"></mdb-card-image>
-							<mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-						</a>
-					</mdb-view>
-					<mdb-btn tag="a" floating action class="ml-auto mr-4" color="elegant">
-						<mdb-icon far icon="heart"/>
-					</mdb-btn>
-					<mdb-card-body class="mt-2">
-						<mdb-card-title class="text-white font-weight-bold">Ex Baron Set - Winter Edition</mdb-card-title>
-						<hr />
-						<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Helm : </span> Snow Baron Helm</p>
-						<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Mail : </span> Snowbaron Mail</p>
-						<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Arms : </span> Snowbaron Vambraces</p>
-						<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Waist : </span> Snowbaron Faulds</p>
-						<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Greaves : </span> Snowbaron Greave</p>
-					</mdb-card-body>
-					<mdb-card-footer color="elegant-color" class="lighten-3 p-0 text-center">
-						<ul class="list-unstyled list-inline font-small mt-3">
-							<li class="list-inline-item pr-2 white-text">
-								<mdb-icon icon="user-astronaut" class="pr-1" /> Kyle Hemsworth
-							</li>
-							<li class="list-inline-item pr-2 white-text">
-								<mdb-icon far icon="clock" class="pr-1" />05/10/2015
-							</li>
-						</ul>
-					</mdb-card-footer>
-				</mdb-card>
-			</mdb-col>
-		</mdb-row>
-	</mdb-container>
+	<mdb-col class="pb-5">
+		<mdb-card class="bg-dark text-light">
+			<mdb-view hover>
+				<a href="#!">
+					<mdb-card-image src="https://i.ibb.co/3MNqtqX/Dr12aio-Vs-AATR-T.jpg" alt="Card image cap"></mdb-card-image>
+					<mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
+				</a>
+			</mdb-view>
+			<mdb-btn tag="a" floating action class="ml-auto mr-4" color="elegant">
+				<mdb-icon far icon="heart"/>
+			</mdb-btn>
+			<mdb-card-body class="mt-2">
+				<mdb-card-title class="text-white font-weight-bold">{{title}}</mdb-card-title>
+				<hr />
+				<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Helm : </span> {{helm}} </p>
+				<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Mail : </span> {{mail}} </p>
+				<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Arms : </span> {{arms}} </p>
+				<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Waist : </span> {{waist}} </p>
+				<p class="font-small grey-dark-text mb-0"> <span class="font-weight-bold">Greaves : </span> {{greaves}} </p>
+			</mdb-card-body>
+			<mdb-card-footer color="elegant-color" class="lighten-3 p-0 text-center">
+				<ul class="list-unstyled list-inline font-small mt-3">
+					<li class="list-inline-item pr-2 white-text">
+						<mdb-icon icon="user-astronaut" class="pr-1" /> {{author}}
+					</li>
+					<li class="list-inline-item pr-2 white-text">
+						<mdb-icon far icon="clock" class="pr-1" /> {{date}}
+					</li>
+				</ul>
+			</mdb-card-footer>
+		</mdb-card>
+	</mdb-col>
 </template>
 
 <script>
-	import { mdbContainer, mdbRow, mdbCol, mdbCard, mdbCardImage, mdbCardBody, mdbCardTitle, mdbCardFooter, mdbBtn, mdbView, mdbMask, mdbIcon } from 'mdbvue';
+	import { mdbCol, mdbCard, mdbCardImage, mdbCardBody, mdbCardTitle, mdbCardFooter, mdbBtn, mdbView, mdbMask, mdbIcon } from 'mdbvue';
 	export default {
 		name: 'FashionCard',
+		props: {
+			title:{
+				type: String
+			},
+			helm:{
+				type: String
+			},
+			mail:{
+				type: String
+			},
+			arms:{
+				type: String
+			},
+			waist:{
+				type: String
+			},
+			greaves:{
+				type: String
+			},
+			author:{
+				type: String
+			},
+			date:{
+				type: String
+			},
+		},
 		components: {
-			mdbContainer,
-			mdbRow,
 			mdbCol,
 			mdbCard,
 			mdbCardImage,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div>
     <Navbar/>
     <div class="container mt-5">
-      <div class="row">
-        <Fashion-Card/>
+      <div class="row" v-for="item in items" :key="item">
+        <Fashion-Card :title="item.title" :helm="item.helm" :mail="item.mail" :arms="item.arms" :waist="item.waist" :greaves="item.greaves" :author="item.author" :date="item.date"/>
       </div>
     </div> 
   </div>
@@ -18,6 +18,22 @@ export default {
   components: {
     Navbar,
     FashionCard
+  },
+  data() {
+    return {
+      items: [
+        {
+          title : "Ex Baron Set - Winter Edition",
+          helm : "Snowbaron Helm",
+          mail : "Snowbaron Mail",
+          arms : "Snowbaron Vambraces",
+          waist : "Snowbaron Faulds",
+          greaves : "Snowbaron Greave",
+          author : "Kyle Hemsworth",
+          date : "12/02/2021"
+        }
+      ]
+    }
   }
 }
 </script>
